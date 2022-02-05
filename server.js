bbee77460a7f93d83c830f84b978775fa0881e59
Marc <cuencamarc96@gmail.com>
2022-02-05 12:17:57 +0100

    Feature: Endpoint testing

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 var cors = require('cors')
+const fetch = require('isomorphic-fetch')
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello, I am the server' });
-});
+app.get('/api/v1/members', (req, res) => {
+  const response = fetch('http://work.mediasmart.io/').then(async response => {
+    const data = await response.text()
+    console.log(data)
+  })
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  res.send({ response })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
